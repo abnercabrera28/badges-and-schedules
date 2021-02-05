54cdb88caaeb708e76f7214651e66f7867cc8380
Abner <abnercabrera28@gmail.com>
2021-02-05 16:18:22 -0500

    commit tree 9d12ffe73e345cced4112f4fe3be15134028d257

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    badge = []
+    array.each do |name|
+        badge.push(badge_maker(name))
+    end
+    return badge
+end
+
+def assign_rooms(array)
+    rooms = []
+    array.each_with_index do |name, room|
+        rooms.push("Hello, #{name}! You'll be assigned to room #{room + 1}!")
+    
+    end
+    rooms
+
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |name|
+        puts name
+    end
+    assign_rooms(array).each do |room|
+        puts room
+    end
+
+end
